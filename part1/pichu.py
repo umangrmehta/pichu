468bb6905e0f77ce27ed6ffac2d60cad7d3b595d
VAISHNAVI\vaishnavi <vsriniv@iu.edu>
2017-10-15 21:00:50 -0400

    pichu with more action

--- part1/pichu.py
+++ part1/pichu.py
@@ -1,6 +1,20 @@
+def move():
+	#generate moves
+	pass
+
+def undoMove():
+	#undo move
+	pass
+
+def action():
+	#each successor for pawn, rook, knight, queen, king, bishop
+	pass
+
+def hueristic():
+	#generate hueristic for each action
+	pass
+
 inputBoard=[]
 initialPosition="RNBQKBNRPPPPPPPP................................pppppppprnbqkbnr"
 for counter in range(0,len(initialPosition),1):
 	inputBoard.append(initialPosition[counter])
-
-print (inputBoard)
