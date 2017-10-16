de7377432a6ddafeee9b73e32908c42ddd9c5e3d
VAISHNAVI\vaishnavi <vsriniv@iu.edu>
2017-10-15 20:56:45 -0400

    pichu to the rescue!!

---
+++ part1/pichu.py
@@ -0,0 +1,6 @@
+inputBoard=[]
+initialPosition="RNBQKBNRPPPPPPPP................................pppppppprnbqkbnr"
+for counter in range(0,len(initialPosition),1):
+	inputBoard.append(initialPosition[counter])
+
+print (inputBoard)
