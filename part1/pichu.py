c11494b66b7f40816ffc00207305cdb5fcfaa81d
Vaishnavi Srinivasan <vsriniv@silo.soic.indiana.edu>
2017-10-22 19:53:39 -0400

    Completed changes

--- part1/pichu.py
+++ part1/pichu.py
@@ -61,7 +61,15 @@ def onBoard(x, y):
 		return True
 	return False
 
-
+#References: https://en.wikipedia.org/wiki/Chess_piece_relative_value#Alternative_valuations
+#
+#The chess pieces have a relative value system. Each piece is assigned a point value to assess the relative strength in tournaments. These pieces are valuated strategically. They are used in computer chess to help the computer evaluate positions.
+#
+#We have used the references and "used by a computer" under "Source" field to assign values to our pieces.
+#
+#We have not assigned any values to for the king.
+#
+#It gives justifiied importace to quentzel as it is the most value for it. Also, as per the program requirements, the parakeet can be modified to a quentzel.
 def evaluation(board):
 	if isTerminal(board):
 		if firstPlayer == 'w':
